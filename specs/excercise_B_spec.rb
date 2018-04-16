3f2ad04f3ad5e73b38d9af9017b5e615d798bd33
Luis Tejero <luisfaridta@gmail.com>
2018-04-16 20:09:20 +0100

    More refactoring for excercise B

--- specs/excercise_B_spec.rb
+++ specs/excercise_B_spec.rb
@@ -3,62 +3,51 @@ require('minitest/rg')
 require_relative('../excercise_B')
 
 class SportTeamTest < MiniTest::Test
+  def setup
+    @team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
+  end
 
   def test_get_team_name
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    assert_equal("Hearts", team1.team_name())
+    assert_equal("Hearts", @team1.team_name())
   end
 
   def test_get_team_players
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    assert_equal(["luis", "colin"], team1.players)
+    assert_equal(["luis", "colin"], @team1.players)
   end
 
   def test_get_team_coach
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    assert_equal("Sandy", team1.coach)
+    assert_equal("Sandy", @team1.coach)
   end
 
   def test_set_team_coach
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    team1.coach = 'John'
-    assert_equal("John", team1.coach)
+    @team1.coach = 'John'
+    assert_equal("John", @team1.coach)
   end
 
   def test_add_a_player
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    team1.add_team_player("Finn")
-    assert_equal(["luis", "colin", "Finn"], team1.players)
+    @team1.add_team_player("Finn")
+    assert_equal(3, @team1.players.length)
   end
 
   def test_find_a_player
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    team_players = team1.find_a_player("luis")
-    assert_equal(true, team_players)
+    assert_equal(true, @team1.find_a_player("luis"))
   end
 
   def test_find_a_player_not_found
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    team_players = team1.find_a_player("Bran")
-    assert_equal(false, team_players)
+    assert_equal(false, @team1.find_a_player("Bran"))
   end
 
   def test_team__won
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    team_players = team1.update_team_points("win")
+    team_players = @team1.update_team_points("win") #left it in this way as future references of pre-refactoring
     assert_equal(3, team_players)
   end
 
   def test_team__lose
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    team_players = team1.update_team_points("lose")
-    assert_equal(0, team_players)
+    assert_equal(0, @team1.update_team_points("lose"))
   end
 
   def test_team__draw
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    team_players = team1.update_team_points("draw")
-    assert_equal(1, team_players)
+    assert_equal(1, @team1.update_team_points("draw"))
   end
 
 
